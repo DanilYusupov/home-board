113f3b9a67c01f0325733eaf30557020f43952b6
Yusupov Danil <danil.usupov@icl-services.com>
2018-10-03 19:09:48 +0300

    Simple vision of task adding

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
+import React from "react";
+import {render} from "react-dom";
+import {Provider} from "react-redux";
 import store from "./store/index";
-import {addTask} from './actions';
+import App from "./components/App";
 
-window.store = store;
-window.addTask = addTask;
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById("react-app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/templates/index.html",
