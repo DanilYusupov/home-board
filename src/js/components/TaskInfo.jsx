777a2b3fba8f2dd5ab81c736f75d10da1e809a72
Yusupov Danil <danil.usupov@icl-services.com>
2018-10-16 19:03:40 +0300

    Added info

--- src/js/components/TaskInfo.jsx
+++ src/js/components/TaskInfo.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import {Modal, ModalBody, ModalHeader, Button} from 'reactstrap'
+import { Modal, ModalBody, ModalHeader, Button } from 'reactstrap'
 
 class TaskInfo extends React.Component {
 
+    status(status) {
+        switch (status) {
+            case 'TODO':
+                return <span className="badge badge-dark">TODO</span>
+            case 'IN_PROGRESS':
+                return <span className="badge badge-warning">IN PROGRESS</span>
+            case 'DONE':
+                return <span className="badge badge-success">DONE</span>
+            default:
+                return <span className="badge badge-danger">Invalid status</span>
+        }
+    }
+
     render() {
-        const {show, toggle, task} = this.props
+        const { show, toggle, task } = this.props
         return (
             <div>
                 <Modal isOpen={show} toggle={toggle}>
@@ -12,7 +25,15 @@ class TaskInfo extends React.Component {
                         {task.title}
                     </ModalHeader>
                     <ModalBody>
-                        {task.text}
+                        <div>
+                            Assignee: <b>{task.assignee}</b>
+                        </div>
+                        <div>
+                            Status: {this.status(task.status)}
+                        </div>
+                        <div>
+                            <p>Description: {task.text}</p>
+                        </div>
                     </ModalBody>
                 </Modal>
             </div>
